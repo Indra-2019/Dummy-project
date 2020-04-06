2fd7bdedf961f881e2539e6493774eede2b07cc1
Indra Kishore <Indra Kishore@DESKTOP-KUL60JH>
2020-04-07 00:09:52 +0530

    Data Types

---
+++ HelloWorld/src/test.java
@@ -0,0 +1,31 @@
+
+public class test {
+	public static void main(String[] args) {
+		
+		
+		
+		byte x=2;	// 1 byte=8bits....max -128 to 127
+		System.out.println(x);
+		
+		int age=10;
+		
+		float rateOfInterest=3.5f;
+		
+		double rate=3.1428;
+		
+		boolean isThisSeriesCool = false;
+		
+		char myCharacter= 'a';
+		
+		
+		System.out.println(x);
+		System.out.println(age);
+		System.out.println(rateOfInterest);
+		System.out.println(myCharacter);
+		System.out.println(rate);
+		System.out.println(age);
+		
+
+	}
+
+}
